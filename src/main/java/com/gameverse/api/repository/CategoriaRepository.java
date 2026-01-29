112b95d66bbd5eed177a6ffbb9da370b969b4ecb
ElGolondras <jfoncla2@institutodh.net>
2026-01-29 23:45:44 +0100

    Contenido Actualizado

--- src/main/java/com/gameverse/api/repository/CategoriaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.gameverse.api.repository;
-
-import com.gameverse.api.entity.Categoria;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
-}
