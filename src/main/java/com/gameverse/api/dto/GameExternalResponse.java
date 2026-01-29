de07dd397985c5323e4746c39b6504634b8dc68a
ElGolondras <jfoncla2@institutodh.net>
2026-01-29 21:17:12 +0100

    API external arreglado

---
+++ src/main/java/com/gameverse/api/dto/GameExternalResponse.java
@@ -0,0 +1,23 @@
+package com.gameverse.api.dto;
+
+import java.util.List;
+import java.util.Map;
+
+
+/**
+ * Esta clase ...
+ *
+ * @Autor: Javier Raposo Huelva
+ * @Version: 2026:01
+ */
+public class GameExternalResponse {
+    private Map<String, Object> data;
+
+    public Map<String, Object> getData() {
+        return data;
+    }
+
+    public void setData(Map<String, Object> data) {
+        this.data = data;
+    }
+}
